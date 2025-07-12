a090c375ef2ba90850dce71f4f4c0fa2449a7ef0
Yann Nóbrega <yann.nicholas@gmail.com>
2025-07-11 23:36:35 -0300

    smaller images

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/medicina110-dev/",
-  plugins: [vue(), tailwindcss()],
+  // base: "/medicina110-dev/",
+  plugins: [vue(), tailwindcss(), ViteImageOptimizer()],
   resolve: {
     alias: [],
   },
