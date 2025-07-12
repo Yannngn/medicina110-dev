b3b0ccb861070b9ed847205b62dff89745fd80fd
Yann Nóbrega <yann.nicholas@gmail.com>
2025-07-11 22:28:59 -0300

    updating

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/medicina110-dev/",
   plugins: [vue(), tailwindcss()],
   resolve: {
-    alias: []},
+    alias: [],
+  },
 });
